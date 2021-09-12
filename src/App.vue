a81bf1a5ec6238d356ade9dcbfd7bd8c6a5179b8
Brian Izaki <bkizaki@outlook.com>
2021-09-12 00:35:34 -0300

    config  inicial
    - limpado arquivos padrões
    - instalado lib para componentes PrimeVue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Cálculo do IMC</h1>
+
+  <h2>Digite seu peso e altura para calcular o IMC</h2>
+
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
 
@@ -19,8 +17,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
